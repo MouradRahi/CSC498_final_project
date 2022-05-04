9f1b9dd3cb56274f5492f7e5b1771f015202dc2c
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-05-04 22:39:10 +0300

    this is my second commit to be honest i don't rememebr what i changed exactly and this is due to the fact i was not ablr to upload to github until today so i zipped the files when i made enough progress so that i can upload them when i get the chance and this is because i didn't rememebr my git password and also the fact that the laptop i am using did not have git on it and i kept forgetting to download it

--- frontend/FinalProject/app/src/main/java/com/example/final_project/CallPage.java
+++ frontend/FinalProject/app/src/main/java/com/example/final_project/CallPage.java
@@ -1,43 +1,27 @@
 package com.example.final_project;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+        import com.google.android.material.snackbar.Snackbar;
 
-import com.google.android.material.navigation.NavigationBarView;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.view.View;
+
+        import androidx.navigation.NavController;
+        import androidx.navigation.Navigation;
+        import androidx.navigation.ui.AppBarConfiguration;
+        import androidx.navigation.ui.NavigationUI;
+
 
 public class CallPage extends AppCompatActivity {
 
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_page);
-
-        String[] arraySpinner = new String[] {
-                "1", "2", "3", "4", "5", "6", "7"
-        };
-        Spinner s = (Spinner) findViewById(R.id.spinner3);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-                // TODO Auto-generated method stub
-                Toast.makeText(getBaseContext(), arraySpinner[position], Toast.LENGTH_SHORT).show();
-            }
-
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
+        setContentView(R.layout.activity_contact_someone);
     }
+
+
 }
