99cae8b43b969f3e8569347619bb014abf823c6d
Mourad Rahi <mouradrahidf1234d@gmail.com>
2022-05-04 22:53:46 +0300

    fifth commit

--- frontend/FinalProject/app/src/main/java/com/example/final_project/MoC.java
+++ frontend/FinalProject/app/src/main/java/com/example/final_project/MoC.java
@@ -2,36 +2,13 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MoC extends AppCompatActivity {
 
-    private Button call;
-    private Button msg;
-    private Button both;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mo_c);
-        call=findViewById(R.id.willing_to_call);
-        msg=findViewById(R.id.willing_to_msg);
-        both=findViewById(R.id.both);
-
-    }
-    public void done(View v){
-        startActivity(new Intent(getApplicationContext(), UserProfile.class));
-    }
-    public void call(View v){
-        Toast.makeText(this, "you have chosen to only receive calls", Toast.LENGTH_LONG).show();
-    }
-    public void msg(View v){
-        Toast.makeText(this, "you have chosen to only receive messages", Toast.LENGTH_LONG).show();
-    }
-    public void both(View v){
-        Toast.makeText(this, "you have chosen to receive both calls and messages", Toast.LENGTH_LONG).show();
     }
 }
--- frontend/FinalProject/app/src/main/java/com/example/final_project/UserProfile.java
+++ frontend/FinalProject/app/src/main/java/com/example/final_project/UserProfile.java
@@ -5,27 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class UserProfile extends AppCompatActivity {
-
-    private Button nm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        nm=findViewById(R.id.no_mind);
     }
     public void goBackToMenu(View v){
         startActivity(new Intent (getApplicationContext(), Menu.class));
-    }
-    public void noMind(View v){
-        startActivity(new Intent (getApplicationContext(), MoC.class));
-    }
-    public void Mind(View v){
-        Toast.makeText(this, "you have chosen to not receive calls and messages", Toast.LENGTH_LONG).show();
     }
 
 
